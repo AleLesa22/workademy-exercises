e72ce124a1b274dc1ce07eee3d9165c37f0046fd
Aleksandar Vidakovic <aleksandar.vidakovic7422@gmail.com>
2022-11-10 14:42:09 +0100

    FirstNonRepeatedCharacter

--- Workademy-Exercises/Workademy_Exercises/FirstNonRepeatedCharacter.cs
+++ Workademy-Exercises/Workademy_Exercises/FirstNonRepeatedCharacter.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Workademy_Exercises
+﻿namespace Workademy_Exercises
 {
-    internal class FirstNonRepeatedCharacter
+    public class FirstNonRepeatedCharacter
     {
+        public static int Test(string str)
+        {
+            List<char> listOfChars = new List<char>();
+            int counter = 0;
+            foreach (char c in str)
+            {
+                listOfChars.Add(c);
+            }
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = 0; j < listOfChars.Count; j++)
+                {
+                    if (str[i] == str[j])
+                    {
+                        counter++;
+                    }
+                }
+                if (counter < 2)
+                {
+                    return i;
+                    break;
+                }
+                else
+                {
+                    counter = 0;
+                }
+            }
+            return -1;
+        }
     }
 }
