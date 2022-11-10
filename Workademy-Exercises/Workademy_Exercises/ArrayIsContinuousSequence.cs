f6061792a8459621334d6eab9e37e146c94fc352
Aleksandar Vidakovic <aleksandar.vidakovic7422@gmail.com>
2022-11-10 17:11:05 +0100

    ArrayIsContinuousSequence

---
+++ Workademy-Exercises/Workademy_Exercises/ArrayIsContinuousSequence.cs
@@ -0,0 +1,65 @@
+﻿namespace Workademy_Exercises
+{
+    public class ArrayIsContinuousSequence
+    {
+        public static bool Test(int[] arr)
+        {
+            bool IsDescending;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] != arr[i + 1])
+                {
+                    if (arr[i] <= arr[i + 1])
+                    {
+                        IsDescending = true;
+                        break;
+                    }
+                    else
+                    {
+                        IsDescending = false;
+                        break;
+                    }
+                }
+            }
+
+            if (IsDescending = true)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if (arr[i] != arr[i + 1])
+                    {
+                        if (arr[i] < arr[i + 1])
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            if (IsDescending = false)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if (arr[i] != arr[i + 1])
+                    {
+                        if (arr[i] > arr[i + 1])
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
