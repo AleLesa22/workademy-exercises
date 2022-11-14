723b7d4eee12e9be78e84900fe7ee4604ea7be6c
Aleksandar Vidakovic <aleksandar.vidakovic7422@gmail.com>
2022-11-14 09:53:50 +0100

    FibonacciNumberCalculator

---
+++ Workademy-Exercises/Workademy_Exercises/FibonacciNumberCalculator.cs
@@ -0,0 +1,23 @@
+﻿namespace Workademy_Exercises
+{
+    public class FibonacciNumberCalculator
+    {
+        public static int Test(int n)
+        {
+            int result = 0;
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n > 2)
+            {
+                result = Test(n - 2) + Test(n - 1);
+            }
+            else
+            {
+                result = 1;
+            }
+            return result;
+        }
+    }
+}
