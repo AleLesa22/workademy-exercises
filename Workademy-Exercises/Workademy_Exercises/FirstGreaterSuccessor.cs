14b80a841a56412dd001817b1fd73aec65cd0dd1
Aleksandar Vidakovic <aleksandar.vidakovic7422@gmail.com>
2022-11-17 08:55:33 +0100

    FirstGreaterSuccessor

---
+++ Workademy-Exercises/Workademy_Exercises/FirstGreaterSuccessor.cs
@@ -0,0 +1,29 @@
+﻿namespace Workademy_Exercises
+{
+    public class FirstGreaterSuccessor
+    {
+        private static int[] Test(int[] initialArray)
+        {
+            int resultIndexJ = 0;
+            int resultIndexI = 0;
+
+            int[] resultArray = new int[initialArray.Length];
+
+            for (int i = 0; i < initialArray.Length; i++)
+            {
+                for (int j = i + 1; j < initialArray.Length; j++)
+                {
+                    if (initialArray[j] > initialArray[i])
+                    {
+                        resultIndexJ = j;
+                        resultIndexI = i;
+
+                        resultArray[i] = resultIndexJ - resultIndexI;
+                        break;
+                    }
+                }
+            }
+            return resultArray;
+        }
+    }
+}
