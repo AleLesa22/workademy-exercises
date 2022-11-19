fa163c54d8e4bb15893644864e26f7d07ba4ba83
AleLesa22 <aleksandar.vidakovic7422@gmail.com>
2022-11-20 00:16:11 +0100

    UglyNumberCheck

---
+++ Workademy-Exercises/Workademy_Exercises/UglyNumberCheck.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workademy_Exercises
+{
+    public class UglyNumberCheck
+    {
+        public static bool Test(int num)
+        {
+            if (num < 0)
+            {
+                return false;
+            }
+            if (num == 1)
+            {
+                return true;
+            }
+
+            while (num != 1)
+            {
+                if (num % 2 == 0)
+                {
+                    while (num % 2 == 0)
+                    {
+                        num = num / 2;
+                        if (num == 1)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                else if (num % 3 == 0)
+                {
+                    while (num % 3 == 0)
+                    {
+                        num = num / 3;
+                        if (num == 1)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                else if (num % 5 == 0)
+                {
+                    if (num % 5 == 0)
+                    {
+                        while (num % 5 == 0)
+                        {
+                            num = num / 5;
+                            if (num == 1)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
